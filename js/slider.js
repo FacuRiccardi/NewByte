8356e27c23c94127a842caf793fe9f5e547ebdae
FacuRiccardi <riccardifacu@gmail.com>
2017-07-26 11:43:54 -0300

    Add Seccion Web y arreglo de Clientes

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.sectionClientes__slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
-});
